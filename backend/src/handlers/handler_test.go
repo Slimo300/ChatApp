7d63901adc64327a005cc5c493980b0676b658bb
Sebastian Ślimak <sebastian.slimak707@gmail.com>
2022-02-27 13:59:53 +0100

    mock fixing

--- backend/src/handlers/handler_test.go
+++ backend/src/handlers/handler_test.go
@@ -93,3 +93,77 @@ func TestRegister(t *testing.T) {
 		})
 	}
 }
+
+func TestSignIn(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	mock := database.NewMockDB()
+	s, err := handlers.NewServer(mock)
+	if err != nil {
+		t.Errorf("Couldn't create a server")
+	}
+	testCases := []struct {
+		desc               string
+		err                bool
+		user               models.User
+		expectedStatusCode int
+		expectedResponse   interface{}
+	}{
+		{
+			desc:               "loginsuccess",
+			err:                false,
+			user:               models.User{Email: "mal.zein@email.com", Pass: "test"},
+			expectedStatusCode: http.StatusOK,
+			expectedResponse:   models.User{Email: "mal.zein@email.com"},
+		},
+		{
+			desc:               "logininvalidemail",
+			err:                true,
+			user:               models.User{Email: "mal.zein@email.com1", Pass: "test"},
+			expectedStatusCode: http.StatusBadRequest,
+			expectedResponse:   gin.H{"err": "not a valid email"},
+		},
+		{
+			desc:               "logininvalidpass",
+			err:                true,
+			user:               models.User{Email: "mal.zein@email.com", Pass: "passeord"},
+			expectedStatusCode: http.StatusBadRequest,
+			expectedResponse:   gin.H{"err": "invalid password"},
+		},
+		{
+			desc:               "loginnosuchuser",
+			err:                true,
+			user:               models.User{Email: "johnny@net.pl", Pass: "password"},
+			expectedStatusCode: http.StatusBadRequest,
+			expectedResponse:   gin.H{"err": "not a valid email"},
+		},
+	}
+
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			url := fmt.Sprintf("/api/login?&email=%s&password=%s", tC.user.Email, tC.user.Pass)
+			req := httptest.NewRequest(http.MethodPost, url, nil)
+			w := httptest.NewRecorder()
+			_, engine := gin.CreateTestContext(w)
+			engine.Handle(http.MethodPost, "/api/login", s.SignIn)
+			engine.ServeHTTP(w, req)
+			response := w.Result()
+
+			if response.StatusCode != tC.expectedStatusCode {
+				t.Errorf("Received Status code %d does not match expected status %d", response.StatusCode, tC.expectedStatusCode)
+			}
+			var respBody interface{}
+			if tC.err {
+				var errmsg gin.H
+				json.NewDecoder(response.Body).Decode(&errmsg)
+				respBody = errmsg
+			} else {
+				var user models.User
+				json.NewDecoder(response.Body).Decode(&user)
+				respBody = models.User{Email: user.Email}
+			}
+			if !reflect.DeepEqual(respBody, tC.expectedResponse) {
+				t.Errorf("Received HTTP response body %+v does not match expected HTTP response Body %+v", respBody, tC.expectedResponse)
+			}
+		})
+	}
+}
